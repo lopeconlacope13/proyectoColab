7cddf7fdc4624dc48604f49f7e6cb982bddcb5ed
lopeconlacope13 <alopper.daw@gmail.com>
2025-05-06 18:15:03 +0200

    Ultima modi

--- Holamundo/src/HolaMundo10.java
+++ Holamundo/src/HolaMundo10.java
@@ -3,7 +3,7 @@ public class HolaMundo10 {
 	
 	void escribirLinea() {
 		
-		System.out.println("Hola mundo de Álvaro López.. ");
+		System.out.println("Hola mundo de Álvaro López.... ");
 		System.out.println("Hola 2");
 		
 	}
